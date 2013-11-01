a75f78df298eecd7d179af59bcc08e931f353fcf
celogeek <me@celogeek.com>
2013-11-01 11:38:46 +0100

    preinstall requirements

--- lib/App/Jedi/Actions/Check.pm
+++ lib/App/Jedi/Actions/Check.pm
@@ -5,13 +5,57 @@ package App::Jedi::Actions::Check;
 use Moo;
 # VERSION
 use MooX::Options;
+use LWP::Protocol::https;
+use LWP::Simple;
 use feature 'say';
+use Path::Class;
+use Carp;
 
 sub run {
 	my ($self, $bundler) = @_;
 
 	say "Bundle Dir : ", $bundler->bundler_root_dir;
-	say  "Config Dir : ", join(":", @{$bundler->config_dirs});
+	$bundler->bundler_root_dir->mkpath;
+	dir($bundler->bundler_root_dir, 'bin')->mkpath;
+
+	my $perl_check = $bundler->bundler_root_dir->file('PERL_VERSION');
+	if (!-e $perl_check) {
+		$perl_check->spew($^V);
+	} else {
+		my $perl_version = $perl_check->slurp;
+		if ($perl_version ne $^V) {
+			croak 
+			join("\n",
+			  "The current perl version is different than the one use for installation."
+			, "Current : " . $^V
+			, "Install : " . $perl_version
+			, ""
+			, "We have XS installed here, you need to remove and reinstall the bundle dir."
+			);
+		}
+	}
+
+	my $cpanm = $bundler->bundler_root_dir->file('bin','cpanm');
+	if (!-f $cpanm) {
+		say "Fetching cpanm ...";
+		my $cpanm_source = get('http://cpanmin.us');
+		if (length($cpanm_source)) {
+			$cpanm->spew($cpanm_source);
+		} else {
+			croak "Cannot fetch cpanm ...";
+		}
+	}
+
+	say "Installing Jedi requirements ...";
+	system(
+		$^X, $cpanm->stringify, 
+		"--mirror-only", 
+		"--mirror", "http://cpan.celogeek.fr", 
+		"--mirror", "http://cpan.org", 
+		"-l", $bundler->bundler_root_dir, 
+		"-L", $bundler->bundler_root_dir, 
+		"-nq", 
+		"Jedi");
 
 	return;
 }
