e736cb2aad520c46d3585b690c9f542e301a037c
celogeek <me@celogeek.com>
2013-11-01 11:38:56 +0100

    launch jedi apps

--- lib/App/Jedi/Actions/Launch.pm
+++ lib/App/Jedi/Actions/Launch.pm
@@ -6,18 +6,18 @@ use Moo;
 # VERSION
 use MooX::Options;
 use feature 'say';
-use Jedi;
-use Plack::Runner;
+use Module::Runtime qw/use_module/;
 
 option 'name' => (is => 'ro', required => 1, doc => 'name of the app to launch', format => 's');
 
 sub run {
 	my ($self, $bundler) = @_;
 
-	my $jedi = Jedi->new;
+
+	my $jedi = use_module('Jedi')->new;
 	$jedi->road('/', 'Jedi::App::' . $self->name);
 
-	my $plack = Plack::Runner->new;
+	my $plack = use_module('Plack::Runner')->new;
 	return $plack->run($jedi->start);
 }
 
